50e9c49dc3b455a65fa9f5d8e380d2ec8741f2b6
qiuyuzhou <piggytommy@gmail.com>
2011-11-15 20:15:48 +0800

    finish the structure of the website: 1. compass 2. sass 3. haml 4. MongoDB

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.10'
+
+gem 'sqlite3'
+gem 'mongoid', '2.0.0.beta.19'
+gem 'bson_ext'
+gem 'haml'
+gem "compass", ">= 0.11.5"
+gem "sass"
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Loveloser::Application.config.secret_token = '274ff0c50f4a2aedb47ee1a74e1aaffe539e4c4b94713a4af1584747f3a446edcb429dce71fcee0e5355e42e353ade559871ea936058392b4d80a2aba67ad936'
